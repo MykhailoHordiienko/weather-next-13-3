69bfdda2a62e38ecfe0a5ebe4d22475e87e26d54
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2023-05-23 13:37:10 +0300

    add Callout and Stat components

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,5 +1,7 @@
 import { getClient } from '@/apollo-client';
 import fetchWeatherQueries from '../../../../../graphql/queries/fetchWeatherQueries';
+import CallOutCard from '@/components/CallOutCard';
+import StatCard from '@/components/StatCard';
 
 type Props = {
   params: {
@@ -23,12 +25,36 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
   });
 
   const result: IRoot = data.myQuery;
-  console.log(result);
   return (
     <div>
-      WeatherPage {city}
-      {lat}
-      {long}
+      {/* information panel */}
+      <div>
+        <div className="p-5">
+          <div className="pb-5">
+            <h2 className="text-xl font-bold">Todays Overview</h2>
+            <p className="text-sm text-gray-400">
+              Last Updated at:
+              {new Date(result.current_weather.time).toLocaleString()}(
+              {result.timezone})
+            </p>
+          </div>
+          <div>
+            <CallOutCard message="This GPT" />
+          </div>
+          <div>
+            <StatCard
+              title="Max Temp"
+              metric={`${result.daily.temperature_2m_max[0].toFixed(1)}°`}
+              color="yellow"
+            />
+            <StatCard
+              title="Min Temp"
+              metric={`${result.daily.temperature_2m_min[0].toFixed(1)}°`}
+              color="green"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
